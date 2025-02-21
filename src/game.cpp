670c784ebd7a5e877ceec768ab45ba20feab2ff9
luxgile <bisslit.12.2@gmail.com>
2025-02-21 16:28:34 +0100

    Added second paddle

--- src/game.cpp
+++ src/game.cpp
@@ -5,27 +5,25 @@
 
 using namespace common;
 
-Game::Game() { }
+Game::Game() {}
 
 Game::~Game() { CloseWindow(); }
 
-void Game::process_frame(float delta) {
-  world.progress();
-}
+void Game::process_frame(float delta) { world.progress(); }
 
 void Game::init_window(const int screen_width, const int screen_height,
                        const char *title) {
   InitWindow(screen_width, screen_height, title);
+  // For this game the camera will be static, I'm not going to work on a fully fledge camera and local positions.
+  /* SetWindowState(FLAG_WINDOW_RESIZABLE); */
 }
 
-void Game::set_target_fps(const int fps) {
-  SetTargetFPS(fps);
-}
+void Game::set_target_fps(const int fps) { SetTargetFPS(fps); }
 
 void Game::start_game() {
   world.set<flecs::Rest>({});
-  world.import<flecs::stats>();
-  world.import<common::module>();
-  world.import<paddle::module>();
+  world.import <flecs::stats>();
+  world.import <common::module>();
+  world.import <paddle::module>();
   world.script_run_file("flecs/paddles.flecs");
 }
--- src/game.h
+++ src/game.h
@@ -9,6 +9,7 @@ private:
 public:
   Game();
   ~Game();
+
   void init_window(const int screen_width, const int screen_height,
                    const char *title);
   void set_target_fps(const int fps);
